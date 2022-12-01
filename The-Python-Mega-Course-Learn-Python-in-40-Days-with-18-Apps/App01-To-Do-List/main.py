c6ecb76f0e160f03ba23a704919cf88bf084bef2
Andy Golubev <verkomf@gmail.com>
2022-12-01 23:16:27 +0200

    day 3

--- The-Python-Mega-Course-Learn-Python-in-40-Days-with-18-Apps/App01-To-Do-List/main.py
+++ The-Python-Mega-Course-Learn-Python-in-40-Days-with-18-Apps/App01-To-Do-List/main.py
@@ -1,7 +1,19 @@
 # Console App
 
-user_prompt = "Enter a todo:"
+
+action_prompt = "Choose an action:"
+add_prompt = "Enter a ToDo:"
 to_do_list = []
 while True:
-    to_do_list.append(input(user_prompt))
-    print(to_do_list)
+    user_action = input(action_prompt)
+    match user_action:
+        case 'add':
+            to_do_list.append(input(add_prompt))
+            for item in to_do_list:
+                print(item)
+        case 'delete':
+            print('delete item')
+        case 'exit':
+            break
+        case _:
+            print("Type 'add' or 'delete' or 'exit'")
