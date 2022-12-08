f2ff27883be764be9b8cd8f956dff7880a947c20
Andy Golubev <verkomf@gmail.com>
2022-12-08 18:16:49 +0200

    day 10

--- The-Python-Mega-Course-Learn-Python-in-40-Days-with-18-Apps/App01-To-Do-List/main.py
+++ The-Python-Mega-Course-Learn-Python-in-40-Days-with-18-Apps/App01-To-Do-List/main.py
@@ -7,7 +7,7 @@ cases = {
     '4': 'Show',
     '5': 'Exit'
 }
-
+filename = "todofile.txt"
 to_do_list = []
 
 def print_action_prompt():
@@ -20,38 +20,56 @@ def print_all():
     for index, item in enumerate(to_do_list):
         print(f"*\t{index+1} - {item}")
     print("*************************")
+
+def flush_to_disk():
+    try:
+
+        with open(filename, 'w') as todofile:
+            for item in to_do_list:
+                todofile.writelines(item + '\n')
+    except:
+        print(f"ERROR while opening file: {filename}")
+
 def __main__():
 
 
-    with open("todofile.txt", 'r') as todo_file:
-        for line in todo_file.readlines():
-            to_do_list.append(line.replace('\n', ''))
+    # READ file
+    try:
+        with open(filename, 'r') as todo_file:
+            for line in todo_file.readlines():
+                to_do_list.append(line.replace('\n', ''))
+    except:
+        print(f"ERROR while opening file: {filename}")
+        exit(1)
 
+    # ASK user what to do
     add_prompt = "Enter a ToDo:"
 
     while True:
         print_action_prompt()
         user_action = input('---> ')
         match user_action:
+            # ADD #
             case '1':
                 to_do_list.append(input(add_prompt))
 
-                with open('todofile.txt', 'w') as todofile:
-                    for item in to_do_list:
-                        todofile.writelines(item + '\n')
+                flush_to_disk()
+            # DELETE #
             case '2':
                 num = int(input("type a todo number to  delete: "))
                 deleted_item = to_do_list.pop(num-1)
                 print(f"Item: {deleted_item.upper()} has been deleted!")
-            # Edit #
+            # EDIT #
             case '3':
                 num = int(input("type a todo number to  edit: "))
                 to_do_list[num - 1] = input("type a new value: ")
                 print_all()
-            # Show #
+            # SHOW #
             case '4':
                 print_all()
+            # EXIT"
             case '5':
+                print("Bye!")
                 break
             case _:
                 print("Type 'add' or 'delete' or 'exit'")
