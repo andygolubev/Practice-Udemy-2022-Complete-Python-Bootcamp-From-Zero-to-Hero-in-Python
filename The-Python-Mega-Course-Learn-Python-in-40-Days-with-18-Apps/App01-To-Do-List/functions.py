e00adf0b4d58fe6cdc351d55f0cc5b914e70798a
Andy Golubev <verkomf@gmail.com>
2022-12-12 23:25:30 +0200

    day 15

---
+++ The-Python-Mega-Course-Learn-Python-in-40-Days-with-18-Apps/App01-To-Do-List/functions.py
@@ -0,0 +1,30 @@
+
+def print_action_prompt():
+    print("Choose an action:")
+    for i, x in cases.items():
+        print(f"{i} - {x}")
+
+def print_all():
+    print("***** List of items *****")
+    for index, item in enumerate(to_do_list):
+        print(f"*\t{index+1} - {item}")
+    print("*************************")
+
+def read_todos_from_file():
+    # READ file
+    try:
+        to_do_list = []
+        with open(filename, 'r') as todo_file:
+            for line in todo_file.readlines():
+                to_do_list.append(line.replace('\n', ''))
+            return to_do_list
+    except:
+        print(f"ERROR while opening file: {filename}")
+        exit(1)
+def flush_to_disk(to_do_list):
+    try:
+        with open(filename, 'w') as todofile:
+            for item in to_do_list:
+                todofile.writelines(item + '\n')
+    except:
+        print(f"ERROR while opening file: {filename}")
