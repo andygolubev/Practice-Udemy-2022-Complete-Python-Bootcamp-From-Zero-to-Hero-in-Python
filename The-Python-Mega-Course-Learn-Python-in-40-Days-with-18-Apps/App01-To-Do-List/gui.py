8d2cd36be546de527afeb0c50971961b8640a362
Andy Golubev <verkomf@gmail.com>
2022-12-21 14:34:04 +0200

    day 17

--- The-Python-Mega-Course-Learn-Python-in-40-Days-with-18-Apps/App01-To-Do-List/gui.py
+++ The-Python-Mega-Course-Learn-Python-in-40-Days-with-18-Apps/App01-To-Do-List/gui.py
@@ -1,2 +1,10 @@
 from modules import functions
 import PySimpleGUI
+import tkinter
+
+label = PySimpleGUI.Text("Type in ToDo:")
+input_text = PySimpleGUI.InputText(tooltip="Enter ToDo")
+
+window = PySimpleGUI.Window('My simple GUI', layout=[[label, input_text]])
+window.read()
+window.close()
